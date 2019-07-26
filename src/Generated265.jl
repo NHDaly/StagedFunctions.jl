f31587bd3d21fd8d8c1fe160e0842d8c315b4258
Nathan Daly <nhdaly@gmail.com>
2019-07-26 15:56:28 -0400

    Cleaned up code: put it in a module

--- src/Generated265.jl
+++ src/Generated265.jl
@@ -1,7 +1,7 @@
-#module Generated265 #end
+module StagedFunctions
 
-const dave = []
-using InteractiveUtils
+export @staged
+
 @assert VERSION >= v"1.3.0-DEV.379"
 # TODO: THIS MUST BE BUILT WITH A MODIFIED VERSION OF JULIA TO EXPORT A NEEDED FUNCTION.
 # YOU CAN CHECKOUT AND BUILD FROM THIS BRANCH:
@@ -9,65 +9,9 @@ using InteractiveUtils
 
 using Cthulhu
 using Cassette # To share their 265 fixing code
+using MacroTools
 
 import Main: @code_lowered
-
-# ----------------
-# -- Adding manual backedges to CodeInfo generator
-# -----------------
-
-_whee() = 2
-_whee()
-f(x) = :($(bar(x)))
-
-function g_gen(self, x)
-    mi = Cthulhu.first_method_instance(f, Tuple{Int})
-    code_info = Core.Compiler.retrieve_code_info(mi)
-    #code_info.edges = [mi]
-    #code_info.edges = [Cthulhu.first_method_instance(_whee, Tuple{})]
-    Core.println("HI")
-    code_info
-end
-@eval function g(x)
-    $(Expr(:meta, :generated_only))
-    $(Expr(:meta,
-        :generated,
-        Expr(:new,
-            Core.GeneratedFunctionStub,
-            :g_gen,
-            Any[:g, :x],
-            Any[],  # spnames
-            @__LINE__,
-            QuoteNode(Symbol(@__FILE__)),
-            true)))
-end
-
-bar(x) = 1
-g(1)
-bar(x) = 3
-##@assert g(1) == 3
-g(1)
-_whee() = whaz()
-whaz() = 1
-g(1)
-whaz() = 3
-g(1)
-
-ci = code_lowered(g, (Int,))[1]
-@show ci.edges
-
-
-# ----------------
-# -- Turning generated expression into a codeinfo
-# -----------------
-
-f(x) = 1
-
-function generatorbody(x)
-    v = f(x)
-    :(x + $v)
-end
-
 
 function expr_to_codeinfo(m, f, t, e)
 
@@ -78,81 +22,26 @@ function expr_to_codeinfo(m, f, t, e)
                 e,
             ))))
 
+    # TODO: Is this right? Should we really be using `Type`, not `Type{Int}`?
     #function_sig = (typeof(f), map(Core.Typeof, t)...)
     function_sig = (typeof(f), (Type for _ in t)...)
-    Core.println("sig: ", function_sig)
     reflection = Cassette.reflect(function_sig)
     ci = reflection.code_info
     ge = Expr(:lambda, ci.slotnames, scoped)
     l = Meta.lower(m, ge)
     ci.code = l.code
+#    Core.println("Before resolving symbols:")
+#    Core.println(ci.code)
     # TODO this requires modifications to Julia to expose jl_resolve_globals_in_ir
     ccall(:jl_resolve_globals_in_ir, Cvoid, (Any, Any, Any), ci.code, @__MODULE__,
             Core.svec(reflection.static_params...)
          )
+#    Core.println("After resolving symbols:")
+#    Core.println(ci.code)
     ci
 end
-ci = expr_to_codeinfo(@__MODULE__, generatorbody, (Int,), generatorbody(Int))
-ci.slotnames
-#println(ci.code)
 
-function g_gen2(self, x)
-    expr = Core._apply_pure(generatorbody,(x,))
-    Core.println(expr)
-    # TODO switch back to expr_to_codeinfo
-    #mi = Cthulhu.first_method_instance(generatorbody, (x,))
-    #code_info = Core.Compiler.retrieve_code_info(mi)
-    # expr_to_codeinfo now sets the edges via Cassette.reflect
-    code_info = expr_to_codeinfo(@__MODULE__, generatorbody, (x,), expr)
-
-    # Add edge to `f` since generatorbody calls `f`.
-    #code_info.edges = [Cthulhu.first_method_instance(f, (x,)),
-    #                   Cthulhu.first_method_instance(generatorbody, (x,))]
-    # TODO: why isn't it enough to pass generatorbody?:
-    #code_info.edges = [Cthulhu.first_method_instance(generatorbody, (x,))]
-    Core.println(code_info.edges)
-    code_info
-end
-@eval function g(x)
-    $(Expr(:meta, :generated_only))
-    $(Expr(:meta,
-        :generated,
-        Expr(:new,
-            Core.GeneratedFunctionStub,
-            :g_gen2,
-            Any[:g, :x],
-            Any[],  # spnames
-            @__LINE__,
-            QuoteNode(Symbol(@__FILE__)),
-            true)))
-end
-
-f(x) = 1
-dump(Core.Compiler.retrieve_code_info(Cthulhu.first_method_instance(g, (Int,))))
-ci = @code_lowered g(1)
-ci.slotnames
-g(1)
-
-f(x) = 9
-
-g(1)
-generatorbody(Int)
-Base.invokelatest(g,1)
-Core._apply_pure(g,1)
-@code_lowered g(1)
-
-#@assert g(1) == 10 # SUCCESS
-
-generatorbody(x) = :(2+$(f(x)))
-g(1)
-
-
-
-# ----------------
-# -- Now do this automatically!
-# ----------------
-
-# ---- Utitlities -------
+# ---- Utilities -------
 """
     argnames(fsig.args[2:end])
 
@@ -185,17 +74,6 @@ function argname(e::Expr)
 end
 # ---------------------
 
-function _delete_prev_methods(f, t)
-    ms = Base.methods(f, t).ms
-    for m in ms
-        if m.sig == Tuple{typeof(f), t...}
-            Base.delete_method(m)
-            return nothing
-        end
-    end
-    return nothing
-end
-
 function _make_generator(f)
     global e = f
     signature = f.args[1]
@@ -213,30 +91,10 @@ function _make_generator(f)
         function $f_stager(self, args...)
             # Within this function, args are types.
 
-            Core.println("ARGS: ", args)
-            #@isdefined($generatorbodyname) && $(@__MODULE__)._delete_prev_methods($generatorbodyname, (args...,));
+            # Call the generatorbody at latest world-age, to avoid currently frozen world-age.
             expr = Core._apply_pure($generatorbodyname, (args...,))
-            Core.println(expr)
-            # TODO switch back to expr_to_codeinfo
-            #mi = Core.Compiler.method_instances($generatorbodyname, Tuple{x})[1]
-            #code_info = Core.Compiler.retrieve_code_info(mi)
-            code_info = expr_to_codeinfo(@__MODULE__, $generatorbodyname, (args...,), expr)
+            code_info = $(@__MODULE__).expr_to_codeinfo(@__MODULE__, $generatorbodyname, (args...,), expr)
 
-            Core.println("BEFORE")
-            Core.println(code_info.edges)
-            Core.println("AFTER")
-
-              # # TODO: Cassette.reflect already adds an edge to (generatorbody, (Int64,)) (for generatorbody(::Type{Int}))
-              # # TODO: Shouldn't it be typeof{Int}
-              # # Add edge to `f` since generatorbody calls `f`.
-               #push!(code_info.edges, Core.Compiler.method_instances($generatorbodyname, Tuple{map(Core.Typeof, args)...})[1])
-               # TODO: DELETE THIS: Manaualy add edge to f
-               #push!(code_info.edges, Core.Compiler.method_instances(f, Tuple{(Type for _ in args)...})[1])
-
-            push!(dave, deepcopy(code_info))
-            Core.println(code_info.edges)
-
-            #code_info.edges = nothing
             code_info
         end;
         function $fname($(fargs...))   # staged function
@@ -261,45 +119,4 @@ macro staged(f)
     _make_generator(f)
 end
 
-@macroexpand @staged function g(x)  :(x + $(f(x)))  end
-@staged function g2(x)
-      f(x)
-end
-
-
-f(x) = 1
-f(1)
-g2(1)
-f(x) = 2
-g2(1)
-
-
-bar(x) = 4
-f(x) = bar(x)
-f(2)
-g2(1)
-bar(x) = 3
-f(2)
-g2(1)
-
-@generated g_norm(x) = :(x + $(f(x)))
-Cassette.@context Ctx;
-Cassette.overdub(Ctx(), g_norm, 1)
-bar(x) = 5
-Cassette.overdub(Ctx(), g_norm, 1)
-
-@staged function g3(x::Int)
-      v = f(x)
-      :(x + $v)
-end
-
-
-g3(1)
-g3(1)
-f(x) = 3
-g3(1)
-f(x) = 8
-@code_typed g3(1)
-
-
-#end # module
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,116 @@
+
+module  M #end
+
+include("../src/Generated265.jl")
+using .StagedFunctions
+
+
+f(x) = 2
+@staged lyndon(x) = f(x)
+
+bodyf = getfield(@__MODULE__, Symbol("##generatorbody#443"))
+
+Main.NHDalyUtils.func_all_specializations(lyndon)
+Main.NHDalyUtils.func_all_specializations(bodyf)
+
+
+
+
+lyndon(2)
+
+Main.NHDalyUtils.func_all_specializations(lyndon)
+Main.NHDalyUtils.func_all_specializations(bodyf)
+
+
+
+f_mi = Core.Compiler.method_instances(f, Tuple{Type{Int}})[1]
+f_mi.backedges
+
+
+dave[end].edges
+f(2)
+f_mi.backedges
+
+#nbits(::Type{T})
+f(x) = 2
+@generated g(x) = f(x)
+f(x) = 6
+g(1)
+bar(x) = f(x)
+bar(2)
+@code_typed bar(2)
+
+
+f(x) = 2
+@staged lyndon(x) = f(x)
+
+lyndon(3)
+f(x) = 3
+lyndon(3)
+@code_typed lyndon(3)
+
+f(x) = nathan(x)
+nathan(x) = 2
+lyndon(1)
+nathan(x::Type{Int}) = "Int"
+nathan(x::Type{<:AbstractFloat}) = "Float"
+lyndon(1)
+lyndon(1.0)
+
+l_mi = Core.Compiler.method_instances(bodyf, Tuple{Type{Int}})[1]
+l_mi.backedges
+
+f_mi = Core.Compiler.method_instances(f, Tuple{Type})[1]
+f_mi.backedges
+Main.NHDalyUtils.func_all_specializations(bodyf)
+Main.NHDalyUtils.func_all_specializations(f)
+
+l_mi == dave[end].edges[1]
+
+
+
+# PROBLEM: There are no backedges from f to bodyf
+# We can cause them by regular invoke
+#invoke(bodyf, Tuple{Type{Int}}, Int)
+bodyf(Int)
+f_mi.backedges
+
+
+foo(x) = bar(x)
+bar(x) = 2
+
+foo(Type{Int})
+Main.NHDalyUtils.func_all_specializations(bar)
+Core.Compiler.method_instances(bar, Type)[1].backedges
+
+
+
+@generated typeargs(x::Vector{T}) where {T} = sizeof(T)
+typeargs([2,3])
+
+
+
+# ============================================================================
+# Comparing with Cassette
+# ============================================================================
+
+using Cassette
+Cassette.@context Ctx;
+
+foo(x) = bar(x)
+bar(x) = x+1
+Cassette.overdub(Ctx(), foo, 2)
+
+foo_mi = Core.Compiler.method_instances(foo, Tuple{Type{Int}})[1]
+foo_mi.backedges
+Main.NHDalyUtils.func_all_specializations(foo)
+
+
+
+
+
+
+@staged lyndon(x) = :(x+1)
+lyndon(2)
+
+end
