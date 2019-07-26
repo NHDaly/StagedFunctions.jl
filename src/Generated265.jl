76fabcedb327b648fc1594ad09e70a3d807ae736
Nathan Daly <nhdaly@gmail.com>
2019-07-26 15:02:18 -0400

    WEEEEEE! FIXED IT!!

--- src/Generated265.jl
+++ src/Generated265.jl
@@ -1,4 +1,7 @@
-module Generated265 #end
+#module Generated265 #end
+
+const dave = []
+using InteractiveUtils
 @assert VERSION >= v"1.3.0-DEV.379"
 # TODO: THIS MUST BE BUILT WITH A MODIFIED VERSION OF JULIA TO EXPORT A NEEDED FUNCTION.
 # YOU CAN CHECKOUT AND BUILD FROM THIS BRANCH:
@@ -42,7 +45,7 @@ end
 bar(x) = 1
 g(1)
 bar(x) = 3
-@assert g(1) == 3
+##@assert g(1) == 3
 g(1)
 _whee() = whaz()
 whaz() = 1
@@ -65,6 +68,7 @@ function generatorbody(x)
     :(x + $v)
 end
 
+
 function expr_to_codeinfo(m, f, t, e)
 
     scoped = Expr(Symbol("scope-block"),
@@ -74,13 +78,16 @@ function expr_to_codeinfo(m, f, t, e)
                 e,
             ))))
 
-    reflection = Cassette.reflect((typeof(f), t...))
+    #function_sig = (typeof(f), map(Core.Typeof, t)...)
+    function_sig = (typeof(f), (Type for _ in t)...)
+    Core.println("sig: ", function_sig)
+    reflection = Cassette.reflect(function_sig)
     ci = reflection.code_info
     ge = Expr(:lambda, ci.slotnames, scoped)
     l = Meta.lower(m, ge)
     ci.code = l.code
     # TODO this requires modifications to Julia to expose jl_resolve_globals_in_ir
-    ccall(:jl_resolve_globals_in    _ir, Cvoid, (Any, Any, Any), ci.code, @__MODULE__,
+    ccall(:jl_resolve_globals_in_ir, Cvoid, (Any, Any, Any), ci.code, @__MODULE__,
             Core.svec(reflection.static_params...)
          )
     ci
@@ -134,7 +141,7 @@ Base.invokelatest(g,1)
 Core._apply_pure(g,1)
 @code_lowered g(1)
 
-@assert g(1) == 10 # SUCCESS
+#@assert g(1) == 10 # SUCCESS
 
 generatorbody(x) = :(2+$(f(x)))
 g(1)
@@ -145,83 +152,100 @@ g(1)
 # -- Now do this automatically!
 # ----------------
 
-using MacroTools
+# ---- Utitlities -------
+"""
+    argnames(fsig.args[2:end])
 
-
-collectcalls(v, out = []) = out
-function collectcalls(e::Expr, out = Any[])
-    if e.head == :call
-        push!(out, e)
-        for a in e.args
-            collectcalls(a, out)
+Return an array of names or gensymed names for each argument in an args list:
+```julia-repl
+julia> argnames(:(f(x::Int, ::Float32, z)).args[2:end])
+3-element Array{Any,1}:
+ :x
+ Symbol("##_1#413")
+ :z
+```
+"""
+function argnames(args::Array)
+    tmpcount = 0
+    out = []
+    for a in args
+        name = argname(a)
+        if name == nothing
+            tmpcount += 1
+            name = gensym("_$tmpcount")
         end
-    elseif e.head == :quote
-        return out
-    else
-        for a in e.args
-            collectcalls(a, out)
-        end
+        push!(out, name)
     end
     out
 end
-
-# TEST
-macro quoted(e) QuoteNode(e) end
-collectcalls(:(a(x) + b(x)))
-collectcalls(@quoted :(x + $(f(x))) )
-@assert length( collectcalls(:(a(x) + b(x))) ) == 3
-
-function call_expr_to_methodinstance_expr(e)
-    argtypes = [:(typeof($a)) for a in e.args[2:end]]
-    :(Cthulhu.first_method_instance($(e.args[1]), ($(argtypes...),)))
+argname(x::Symbol) = (x)
+function argname(e::Expr)
+    @assert e.head == Symbol("::")  "Expected (x::T), Got $e"
+    return length(e.args) == 2 ? (e.args[1]) : nothing
 end
-call_expr_to_methodinstance_expr(:(a(x) + b(x)))
+# ---------------------
 
+function _delete_prev_methods(f, t)
+    ms = Base.methods(f, t).ms
+    for m in ms
+        if m.sig == Tuple{typeof(f), t...}
+            Base.delete_method(m)
+            return nothing
+        end
+    end
+    return nothing
+end
 
 function _make_generator(f)
     global e = f
     signature = f.args[1]
     (fname, fargs) = signature.args[1], signature.args[2:end]
-    body = f.args[2]
 
-    # Collect all calls in f
-    calls = collectcalls(body)
-
-    methodinstances = [call_expr_to_methodinstance_expr(c) for c in calls]
-
+    # Strip type-assertions and gensymed missing names for all args
+    # (x::Int, y, ::Float32) -> (x,y,##genarg##)
+    signature.args = argnames(signature.args)
 
     # Update f to be the generatorbody
-    generatorbodyname = signature.args[1] = :generatorbody
-    f_generator = #=gensym(=# Symbol("$(fname)_generator")
+    generatorbodyname = signature.args[1] = gensym(:generatorbody)
+    f_stager = gensym( Symbol("$(fname)_stager") )
     esc(:(
-        $f;
-        function $f_generator(self, $(fargs...))
-            expr = Core._apply_pure($generatorbodyname, ($(fargs...),))
+        $f;   # user-written generator body function
+        function $f_stager(self, args...)
+            # Within this function, args are types.
+
+            Core.println("ARGS: ", args)
+            #@isdefined($generatorbodyname) && $(@__MODULE__)._delete_prev_methods($generatorbodyname, (args...,));
+            expr = Core._apply_pure($generatorbodyname, (args...,))
             Core.println(expr)
             # TODO switch back to expr_to_codeinfo
-            #mi = Cthulhu.first_method_instance($generatorbodyname, (x,))
+            #mi = Core.Compiler.method_instances($generatorbodyname, Tuple{x})[1]
             #code_info = Core.Compiler.retrieve_code_info(mi)
-            code_info = expr_to_codeinfo(@__MODULE__, $generatorbodyname, ($(fargs...),), expr)
+            code_info = expr_to_codeinfo(@__MODULE__, $generatorbodyname, (args...,), expr)
 
-            # Add edge to `f` since generatorbody calls `f`.
-            # TODO: why isn't it enough to pass generatorbody?:
-            #code_info.edges = [Cthulhu.first_method_instance($generatorbodyname, ($(fargs...),))]
-
-            # For now, manually construct an edge for every function that f calls, by
-            # collecting them from the expression tree (EWWW....)
-            code_info.edges = [Cthulhu.first_method_instance($generatorbodyname, ($(fargs...),)),
-                              $(methodinstances...)
-                               ]
+            Core.println("BEFORE")
             Core.println(code_info.edges)
+            Core.println("AFTER")
+
+              # # TODO: Cassette.reflect already adds an edge to (generatorbody, (Int64,)) (for generatorbody(::Type{Int}))
+              # # TODO: Shouldn't it be typeof{Int}
+              # # Add edge to `f` since generatorbody calls `f`.
+               #push!(code_info.edges, Core.Compiler.method_instances($generatorbodyname, Tuple{map(Core.Typeof, args)...})[1])
+               # TODO: DELETE THIS: Manaualy add edge to f
+               #push!(code_info.edges, Core.Compiler.method_instances(f, Tuple{(Type for _ in args)...})[1])
+
+            push!(dave, deepcopy(code_info))
+            Core.println(code_info.edges)
+
+            #code_info.edges = nothing
             code_info
         end;
-        function $fname($(fargs...))
+        function $fname($(fargs...))   # staged function
             $(Expr(:meta, :generated_only))
             $(Expr(:meta,
                 :generated,
                 Expr(:new,
                     Core.GeneratedFunctionStub,
-                    f_generator,
+                    f_stager,
                     Any[fname, fargs...],
                     Any[],  # spnames
                     @__LINE__,
@@ -231,29 +255,51 @@ function _make_generator(f)
     ))
 end
 
-macro generated265(f)
-    @assert isa(f, Expr) && (f.head === :function || Base.is_short_function_def(f)) "invalid syntax; @generated265 must be used with a function definition"
+macro staged(f)
+    @assert isa(f, Expr) && (f.head === :function || Base.is_short_function_def(f)) "invalid syntax; @staged must be used with a function definition"
 
     _make_generator(f)
 end
 
-@macroexpand @generated265 function g(x)  :(x + $(f(x)))  end
-@generated265 function g2(x)
-      v = f(x)
-      Expr(:call, :+, :x, v)
+@macroexpand @staged function g(x)  :(x + $(f(x)))  end
+@staged function g2(x)
+      f(x)
 end
 
+
+f(x) = 1
+f(1)
+g2(1)
 f(x) = 2
-@assert g2(1)  == 3
-f(x) = 6
-@assert g2(1)  == 7
+g2(1)
 
+
+bar(x) = 4
 f(x) = bar(x)
-bar(x) = 1
-@assert g2(1)  == 2
+f(2)
+g2(1)
 bar(x) = 3
-@assert g2(1)  == 4
+f(2)
+g2(1)
+
+@generated g_norm(x) = :(x + $(f(x)))
+Cassette.@context Ctx;
+Cassette.overdub(Ctx(), g_norm, 1)
+bar(x) = 5
+Cassette.overdub(Ctx(), g_norm, 1)
+
+@staged function g3(x::Int)
+      v = f(x)
+      :(x + $v)
+end
 
 
+g3(1)
+g3(1)
+f(x) = 3
+g3(1)
+f(x) = 8
+@code_typed g3(1)
 
-end # module
+
+#end # module
